6651ac78a0e0f53b4e3a10856ede8044f9c1293c
Shanmuga5 <92845812+Shanmuga5@users.noreply.github.com>
2022-01-08 20:39:24 +0530

    Add files via upload

---
+++ arithmatic operaters.py
@@ -0,0 +1,19 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5*5
+25
+5-(120-50)
+-65
+2*5
+10
+2**5
+32
+width = 100
+height= 8*5
+width*height
+4000
+tamil,english,science = 80,70,80
+tamil+english+science
+230
+salary =50000
+tax = s
