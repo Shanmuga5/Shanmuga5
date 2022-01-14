ac36899a161a99857776e704fa0bd30757439c01
Shanmuga Priyan <92845812+Shanmuga5@users.noreply.github.com>
2022-01-14 20:49:50 +0530

    Add files via upload
    small task

---
+++ new.py
@@ -0,0 +1,8 @@
+mark1 = int(input("please enter mark"))
+mark2 = int(input("please enter mark"))
+mark3 = int(input("please enter any mark"))
+
+if (mark1==mark2) and (mark2==mark3):
+    print("both are same")
+else :
+    print ("both are not same")
